23e8aadef584a847401b14e40d48879804e5e8ef
ZoltVarga <skyspirit86@gmail.com>
2017-12-10 22:19:26 +0100

    Initial Commit

---
+++ SimpleBannerTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SimpleBannerTest
+//
+//  Created by Varga Zolt on 12/10/17.
+//  Copyright © 2017 Varga Zolt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SimpleBannerTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SimpleBannerTest
+//
+//  Created by Varga Zolt on 12/10/17.
+//  Copyright © 2017 Varga Zolt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
